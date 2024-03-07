0ec764cb1c946503a1b471cf64549493ba1396dd
Animesh Pareek <108825332+Mr-AniP@users.noreply.github.com>
2024-03-07 16:25:30 +0530

    Add files via upload

---
+++ tb_noc1.v
@@ -0,0 +1,33 @@
+`include "main.v"
+
+module tb_noc();
+    reg clk=0;
+    reg reset=0;
+    wire [3:0] processor_ready_signals;
+    reg [10:0] p0_configure,p1_configure,p2_configure,p3_configure;
+    mesh m1 (.clock(clk), 
+            .reset(reset),
+            .p0_configure(p0_configure),
+            .p1_configure(p1_configure),
+            .p2_configure(p2_configure),
+            .p3_configure(p3_configure),
+            .processor_ready_signals(processor_ready_signals)
+            );
+    initial
+    begin
+         // $display("in");
+        $dumpfile("noc_sim.vcd");
+        $dumpvars(0,tb_noc);
+        // clock_t=1'b0;
+        reset=1'b0;
+        #1 reset = 1'b1;
+        #16 reset = 1'b0;
+        #2 p0_configure = 11'b01000000101;
+        #60 p0_configure = 11'b01000000011;
+        p3_configure = 11'b01000000001;
+        // #29$finish;
+        #2000 $finish;
+    end
+    always #10 clk = ~clk;
+
+endmodule
