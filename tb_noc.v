78be099694e3986bd8c7d9a7cac1ec7bfef180aa
Animesh Pareek <108825332+Mr-AniP@users.noreply.github.com>
2024-03-13 20:51:35 +0530

    Update tb_noc.v

--- tb_noc.v
+++ tb_noc.v
@@ -4,16 +4,18 @@ module tb_noc();
     reg clk=0;
     reg reset=0;
     wire [3:0] processor_ready_signals;
+    reg block_all_paths;
     reg [10:0] p0_configure,p1_configure,p2_configure,p3_configure;
-    wire [19:0] temp_path_block_signals;
+    //wire [19:0] temp_path_block_signals;
     mesh m1 (.clock(clk), 
             .reset(reset),
             .p0_configure(p0_configure),
             .p1_configure(p1_configure),
             .p2_configure(p2_configure),
             .p3_configure(p3_configure),
+             .block_all_paths(block_all_paths),
             .processor_ready_signals(processor_ready_signals),
-            .temp_path_block_signals(temp_path_block_signals) 
+            //.temp_path_block_signals(temp_path_block_signals) 
             );
     initial
     begin
@@ -22,6 +24,7 @@ module tb_noc();
         $dumpvars(0,tb_noc);
         // clock_t=1'b0;
         reset=1'b0;
+        block_all_paths=1'b0;
         #1 reset = 1'b1;
         #16 reset = 1'b0;
         #2 p0_configure = 11'b00001000011;
